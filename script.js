cd478310283cc699315e8241177900dc2f65b49d
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-23 13:17:19 +0100

    const for loop to play five games

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
@@ -10,39 +12,57 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let playerAnswer = prompt("Write rock, paper, or scissors to play: ");
-    playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1).toLowerCase();
-    return playerAnswer;
+    let playerChoice = prompt("Write rock, paper, or scissors to play: ");
+    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    return playerChoice;
 }
 
-const playerChoice = getPlayerChoice();
-const computerChoice = getComputerChoice();
-
 function playRound(playerChoice, computerChoice) {
-
-    if (
-        (playerChoice === "Rock" && computerChoice === "Scissors") ||
-        (playerChoice === "Paper" && computerChoice === "Rock") ||
-        (playerChoice === "Scissors" && computerChoice === "Paper")
-    ) {
-        return "You won!";
-    }
-    if (
-        (computerChoice === "Rock" && playerChoice === "Scissors") ||
-        (computerChoice === "Paper" && playerChoice === "Rock") ||
-        (computerChoice === "Scissors" && playerChoice === "Paper")
-    ) {
-        return "You lost";
-    }
     if (playerChoice === computerChoice) {
         return "Draw";
+    }
+    if (playerChoice === "Rock") {
+        if (computerChoice === "Paper") {
+            return "You lost";
+        } else {
+            return "You won";
+        }
+    }
+    if (playerChoice === "Paper") {
+        if (computerChoice === "Scissors") {
+            return "You lost"; // scissors wins
+        } else {
+            return "You won";
+        }
+    }
+    if (playerChoice === "Scissors") {
+        if (computerChoice === "Rock") {
+            return "You lost";
+        } else {
+            return "You won";
+        }
     }
     if (playerChoice !== computerChoice) {
         return "Please choose correct choice";
     }
 }
 
-playRound(playerChoice, computerChoice);
-console.log(playerChoice);
-console.log(computerChoice);
-console.log(playRound(playerChoice, computerChoice));
+function game() {
+    let playerChoice, computerChoice;
+    let results = [];
+
+    for (let rounds = 0; rounds < 5; rounds++) {
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
+        if (playerScore < 5 || computerScore < 5) {
+            const result = playRound(playerChoice, computerChoice);
+            playRound(playerChoice, computerChoice);
+            results.push(result);
+            console.log(rounds);
+            console.log(results);
+        }
+    }
+}
+game();
+// console.log(playerScore + " : " + computerScore);
+
